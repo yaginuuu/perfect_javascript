3491c6367c2b3421dbe9ef21872835cd610c6732
yaginuuu <yaginuuu56@gmail.com>
2016-05-25 00:37:41 +0900

    第3章まで終わりました.

---
+++ sec03/script.js
@@ -0,0 +1,21 @@
+// 空のオブジェクト生成
+var obj = {};
+
+// 最短表記の型変換
+var n = 3;
+n+'';
+
+var s = '3';
++s;
+
+// オブジェクトはtureになる
+var b = new Boolean(false);
+if(b) {print('T');} else {print('F');}
+
+var z = new Number(0);
+if(z) {print('T');} else {print('F');}
+
+var s = new String('');
+if(s) {print('T');} else {print('F');}
+
+
